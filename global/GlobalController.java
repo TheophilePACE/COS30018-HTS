e7b9782087da2227382c9bd1fedd5b78ce638cbc
TheophilePACE <theophile.pace@gmail.com>
2017-10-07 12:14:01 +1100

    Adding transmissionAgent creating to homeController. TRigger it from GlobalCOntroller

--- global/GlobalController.java
+++ global/GlobalController.java
@@ -4,13 +4,17 @@ import jade.core.Runtime;
 import jade.core.Profile;
 import jade.core.ProfileImpl;
 import jade.wrapper.*;
+
 import homeAgent.HomeAgentController;
+import brokerAgent.BrokerAgentController;
 
 public class GlobalController {
 	private static String HOME_AGENT_ADDRESS = "homeAgent";
 	private static long CYCLE_TIME = 20000;
 	private static int PORT = 1099;
 	private static String HOST = null;
+	private static String BROKER_ADRESS = "brokerAgent";
+	private static String TRANSMISSION_AGENT_ADDRESS = "transmissionAgent";
 	@SuppressWarnings("unused")
 	private static String PATH_TO_CONFIG = null; //TODO: read the config file and generate the agents
 
@@ -22,11 +26,13 @@ public class GlobalController {
 		log(": Launching the platform Main Container on port "+ PORT +" ...");
 		ProfileImpl pMain = new ProfileImpl(HOST, PORT, null,true);
 		pMain.setParameter(Profile.GUI, "true");
+		@SuppressWarnings("unused")
 		ContainerController mainCtrl = rt.createMainContainer(pMain);
 
 		//Launch homecontainer and agents
-		HomeController homeController = new HomeController(HOME_AGENT_ADDRESS,CYCLE_TIME,rt,HOST,PORT);
+		HomeController homeController = new HomeController(HOME_AGENT_ADDRESS,CYCLE_TIME,rt,HOST,PORT,TRANSMISSION_AGENT_ADDRESS,BROKER_ADRESS);
 		try {
+			homeController.createTransmissionAgent();
 			homeController.createHomeAgent(30, 10);
 			homeController.createHomeAgent(30, 10); //This is an error but it should not crash, thanks to the controller
 			homeController.createAppliance("Appliance1");
@@ -34,7 +40,13 @@ public class GlobalController {
 		} catch (Exception e) {
 			log(e.toString());
 		}
-
+		
+		//Launch a broker and retailers
+		BrokerAgentController brokerAgentController = new BrokerAgentController(CYCLE_TIME, rt, HOST, PORT, BROKER_ADRESS);
+		brokerAgentController.createRetailerAgent("Retail Agent 1", "Retail Agent", "AGL");
+		brokerAgentController.createRetailerAgent("Retail Agent 2", "Retail Agent", "Origin");
+		brokerAgentController.createBrokerAgent();
+//		brokerAgentController.createInitiatorAgent("Initiator Agent");
 	}
 	private static String log(String s) {
 		String toPrint = "[" + HomeAgentController.class.getName() + "] " + s;
--- global/HomeController.java
+++ global/HomeController.java
@@ -7,6 +7,7 @@ package global;
 import jade.core.Runtime;
 import applianceAgent.ApplianceAgent;
 import homeAgent.HomeAgent;
+import homeAgent.TransmissionAgent;
 import jade.core.Profile;
 import jade.core.ProfileImpl;
 import jade.wrapper.*;
@@ -16,17 +17,22 @@ public class HomeController {
 	private long CYCLE_TIME;
 	private ContainerController homeContainer;
 	private AgentController homeAgent;
+	private AgentController transmissionAgent;
+	private String TRANSMISSION_AGENT_ADDRESS;
+	private String BROKER_AGENT_ADDRESS;
 
-	public HomeController(String homeAgentAdress, long cycleTime, Runtime rt, String host, int port) {
+	public HomeController(String homeAgentAdress, long cycleTime, Runtime rt, String host, int port, String transmissionAgentAddress, String brokerAdress) {
 		super();
 		HOME_AGENT_ADDRESS =  homeAgentAdress;
 		CYCLE_TIME = cycleTime;
+		TRANSMISSION_AGENT_ADDRESS = transmissionAgentAddress;
+		BROKER_AGENT_ADDRESS = brokerAdress;
 		Profile pHomeContainer = new ProfileImpl(host,port,null,false); //create a non-main container
-		pHomeContainer.setParameter("CONTAINER_NAME", "homeContainer");
+		pHomeContainer.setParameter(Profile.CONTAINER_NAME, "homeContainer");
 		homeContainer = rt.createAgentContainer(pHomeContainer);
 		log("The homeContainer was created!");
 		homeAgent =null;
-	}
+	} 
 	private AgentController makeCreateAppliance(String agentName, long cycleTime, String homeAgentAdress, ContainerController containerController) {
 		Object[] appArgs = new Object[2];
 		appArgs[0] = cycleTime;
@@ -43,13 +49,15 @@ public class HomeController {
 	public AgentController createAppliance(String agentName) {
 		return makeCreateAppliance(agentName, CYCLE_TIME, HOME_AGENT_ADDRESS, homeContainer);
 	}
-	private AgentController makeCreateHomeAgent(String name,int maxBuyPrice, int minSellPrice, long cycleTime) {
+	private AgentController makeCreateHomeAgent(String name,int maxBuyPrice, int minSellPrice, long cycleTime, String transmissionAgentAdress) {
 		if(homeAgent == null) {
 			try {
-				Object[] homeArgs = new Object[3];
+				Object[] homeArgs = new Object[4];
 				homeArgs[0] = maxBuyPrice; //maxBuyPrice
 				homeArgs[1] = minSellPrice; //minSellPrice
 				homeArgs[2] = cycleTime;
+				homeArgs[3] = transmissionAgentAdress;
+				
 				log("Creating agent Home ");
 				homeAgent = homeContainer.createNewAgent(name, HomeAgent.class.getName(), homeArgs);
 				homeAgent.start();		
@@ -62,9 +70,29 @@ public class HomeController {
 		return homeAgent;
 	}
 	public AgentController createHomeAgent(int maxBuyPrice, int minSellPrice) {
-		return makeCreateHomeAgent(HOME_AGENT_ADDRESS, maxBuyPrice,minSellPrice,CYCLE_TIME);
+		return makeCreateHomeAgent(HOME_AGENT_ADDRESS, maxBuyPrice,minSellPrice,CYCLE_TIME, TRANSMISSION_AGENT_ADDRESS);
 	}
 
+	private AgentController makeCreateTransmissionAgent(String name, String brokerAdress) {
+		if(transmissionAgent == null) {
+			try {
+				Object[] homeArgs = new Object[1];
+				homeArgs[0] = brokerAdress; 
+				log("Creating agent Transmission ");
+				transmissionAgent = homeContainer.createNewAgent(name, TransmissionAgent.class.getName(), homeArgs);
+				transmissionAgent.start();		
+			} catch (Exception e) {
+				log(e.toString());
+			}
+		} else {
+			log("TransmissionAgent already created");
+		}
+		return transmissionAgent;
+	}
+	public AgentController createTransmissionAgent() {
+		return makeCreateTransmissionAgent(TRANSMISSION_AGENT_ADDRESS, BROKER_AGENT_ADDRESS);
+	}
+	
 	private static String log(String s) {
 		String toPrint = "[" + HomeController.class.getName() + "] " + s;
 		System.out.println(toPrint);
