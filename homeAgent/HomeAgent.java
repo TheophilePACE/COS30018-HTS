a313646ab95150c7119d78d3ed30932220a90f7f
TheophilePACE <theophile.pace@gmail.com>
2017-10-07 12:14:50 +1100

    Add communication with transmissionAgent to homeAgent

--- homeAgent/HomeAgent.java
+++ homeAgent/HomeAgent.java
@@ -1,10 +1,12 @@
 package homeAgent;
 
+import jade.core.AID;
 import jade.core.Agent;
 import jade.core.behaviours.CyclicBehaviour;
 import jade.core.behaviours.TickerBehaviour;
 import jade.lang.acl.ACLMessage;
 import jade.lang.acl.MessageTemplate;
+import jade.proto.AchieveREInitiator;
 import jade.domain.FIPANames;
 
 import java.util.Date;
@@ -18,15 +20,17 @@ public class HomeAgent extends Agent {
 	private int energyBalance = 0; //sum of production and consumption of energy
 	private int energyProducted = 0;
 	private int energyConsumed = 0;
-	
+
 	//Limits for dealing. Could be change by the user
 	private int maxBuyingPrice = 0;
 	private int minSellingPrice = 0;
-	
+
 	//TODO Change at every cycle. Provided by the Broker
+	@SuppressWarnings("unused")
 	private int BetterPrice = 0;
+	@SuppressWarnings("unused")
 	private String BetterProvider = "";
-	
+	private String transmissionAgentAddress= "";
 	private HashMap<String, Integer> applianceEnergyBalance = new HashMap<String, Integer>();
 	protected void setup() {
 		//FIPANames.InteractionProtocol.FIPA_REQUEST
@@ -35,6 +39,7 @@ public class HomeAgent extends Agent {
 			maxBuyingPrice= Integer.parseInt(args[0].toString());
 			minSellingPrice=Integer.parseInt(args[1].toString());
 			CYCLE_TIME=Long.parseLong(args[2].toString());
+			transmissionAgentAddress=args[3].toString();
 			log("I have been created. CUSTOM maxBuyingPrice is "+ maxBuyingPrice +" and minSellingPrice is "+ minSellingPrice);
 		} else {
 			log("I have been created. DEFAULT maxBuyingPrice is "+ maxBuyingPrice +" and minSellingPrice is "+ minSellingPrice);
@@ -43,8 +48,8 @@ public class HomeAgent extends Agent {
 		MessageTemplate energyBalanceMessageTemplate = MessageTemplate.and(
 				MessageTemplate.MatchProtocol(FIPANames.InteractionProtocol.FIPA_REQUEST),
 				MessageTemplate.MatchPerformative(ACLMessage.REQUEST));
-		
-		
+
+
 		CyclicBehaviour energyBalanceReceivingBehaviour = (new CyclicBehaviour(this) {
 			public void action() {
 				ACLMessage msg= receive();
@@ -63,18 +68,59 @@ public class HomeAgent extends Agent {
 				block();
 			}
 		}
-				);
+		);
 		addBehaviour(energyBalanceReceivingBehaviour);
-		
-		TickerBehaviour makeBalanceBehaviour = new TickerBehaviour(this,CYCLE_TIME) {
+
+		 
+		addBehaviour(new TickerBehaviour(this,CYCLE_TIME) {
 			public void onTick() {
 				log("Going to compute balance");
-				makeEnergyBalance();
-				log("Consumption: " + energyConsumed + " production: " + energyProducted + " --> BALANCE = " + energyBalance);	
+				int quantity =  makeEnergyBalance();
+				log("Consumption: " + energyConsumed + " production: " + energyProducted + " --> BALANCE = " + quantity);
+				ACLMessage requestToTransmissionAgent = new ACLMessage(ACLMessage.REQUEST);
+				// Add receivers from input args
+				requestToTransmissionAgent.addReceiver(new AID(transmissionAgentAddress, AID.ISLOCALNAME));
+				// Set the interaction protocol
+				requestToTransmissionAgent.setProtocol(FIPANames.InteractionProtocol.FIPA_REQUEST);
+				// Specify the reply deadline (10 seconds)
+				requestToTransmissionAgent.setReplyByDate(new Date(System.currentTimeMillis() + 10000));
+				// Set message content, if quantity >0 we need to buy otherwise sell
+				String contentJSON = "{'quantity':" + quantity + "}";
+
+				requestToTransmissionAgent.setContent(contentJSON);
+				myAgent.addBehaviour(new AchieveREInitiator(myAgent, requestToTransmissionAgent) {
+					protected void handleAgree(ACLMessage agree) {
+						log(agree.getSender().getLocalName() + " has agreed to the request");
+					}
+					
+					// Method to handle an inform message from responder
+					protected void handleInform(ACLMessage inform) {
+						JSONObject response = new JSONObject(inform.getContent());
+						log(inform.getSender().getLocalName() + " successfully performed the request: '" + requestToTransmissionAgent.getContent());
+						log(inform.getSender().getLocalName() + " negotiated price of: '" + response.getDouble("price") + " c/kWh'");
+					}
+
+					// Method to handle a refuse message from responder
+					protected void handleRefuse(ACLMessage refuse) {
+						log(refuse.getSender().getLocalName() + " refused to perform the requested action");
+					}
+
+					// Method to handle a failure message (failure in delivering the message)
+					protected void handleFailure(ACLMessage failure) {
+						if (failure.getSender().equals(myAgent.getAMS())) {
+							// FAILURE notification from the JADE runtime: the receiver (receiver does not exist)
+							log("Responder does not exist");
+						} else {
+							log(failure.getSender().getLocalName() + " failed to perform the requested action");
+						}
+					}
+				
+				
+				});
+				
 			}
-		};
-		addBehaviour(makeBalanceBehaviour);
-		
+		});
+
 	}
 	private void setApplianceEnergyBalance(String applianceName, Integer balance) {
 		applianceEnergyBalance.put(applianceName, balance);
@@ -83,7 +129,7 @@ public class HomeAgent extends Agent {
 	private int makeEnergyBalance() {
 		energyConsumed = 0;
 		energyProducted = 0;
-		
+
 		applianceEnergyBalance.forEach((k,v)->{
 			if(v>0)
 				energyConsumed+=v;
@@ -92,7 +138,7 @@ public class HomeAgent extends Agent {
 			storeApplianceEnergyBalance(k, v); //Store old value
 		});
 		applianceEnergyBalance.clear(); // remove old values
-		
+
 		energyBalance=energyConsumed + energyProducted;
 		return energyBalance;
 	}
@@ -101,7 +147,7 @@ public class HomeAgent extends Agent {
 		Date date = new Date();
 		log("STORE: " +date.toString() + ": device"+ k + "-->" + v +"kW");
 	}
-	
+
 	private String log(String s) {
 		String toPrint = "[" + getLocalName() + "] " + s;
 		System.out.println(toPrint);
