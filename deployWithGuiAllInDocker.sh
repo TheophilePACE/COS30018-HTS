2d00d5ba5c35d2b1f426ba35fe47ba8a6f04b779
TheophilePACE <theophile.pace@gmail.com>
2017-10-25 22:44:07 +1100

    deployscripts open the webbrowser automatically

--- deployWithGuiAllInDocker.sh
+++ deployWithGuiAllInDocker.sh
@@ -2,4 +2,16 @@
 docker-compose down
 docker-compose pull
 docker-compose up -d
+if [ "$(uname)" == "Darwin" ]; then
+    open http://localhost:3000
+    echo "browser opend on localhost:3000!"
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    xdg-open http://localhost:3000
+    echo "browser opend on localhost:3000!"
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
+    start http://localhost:3000
+    echo "browser opend on localhost:3000!"
+else
+    echo "you will have to open your browser yourself !"
+fi
 docker-compose logs -ft jade
